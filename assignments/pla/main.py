35709bb8125da9c870f6a7e3303a162dbd55e131
Brian Bamsch <bbamsch32@gmail.com>
2018-03-03 23:26:59 -0800

    Complete Main Learning/Inference Flows
    Implement Learning + Inference Phase with Num Iterations Target

--- assignments/pla/main.py
+++ assignments/pla/main.py
@@ -1,55 +1,77 @@
 """Runs PLA Learning Algorithm"""
 
-import sys
-
 from absl import app
 from absl import flags
 from absl import logging
-from matplotlib import pyplot as plt
 from generator.bernoulli import LinearBernoulliGenerator, RandomBernoulliGenerator
 from algorithm import PLA
 
 FLAGS = flags.FLAGS
 
+flags.DEFINE_enum(
+    name='generator',
+    default='linear',
+    enum_values=['linear', 'random'],
+    help='Data generator to use')
+flags.DEFINE_integer(
+    name='learning_points',
+    default=1000,
+    help='Number of Data points used during learning')
+flags.DEFINE_integer(
+    name='num_iter',
+    default=1000,
+    help='Number of PLA iterations to run during training')
+flags.DEFINE_integer(
+    name='inference_points',
+    default=1000,
+    help='Number of Data points to classify during inference')
+
 
 def main(argv):
     del argv  # Unused
 
-    logging.info('Running under Python {0[0]}.{0[1]}.{0[2]}'.format(sys.version_info))
+    logging.info('====== LEARNING ======')
+    logging.info('Data Generator => {0}'.format(FLAGS.generator))
+    logging.info('Num Data Points => {0}'.format(FLAGS.learning_points))
+    logging.info('Num Iterations => {0}'.format(FLAGS.num_iter))
+
     generator = LinearBernoulliGenerator()
-    data = generator.generate(1000)
+    if FLAGS.generator == 'linear':
+        generator = LinearBernoulliGenerator()
+    elif FLAGS.generator == 'random':
+        generator = RandomBernoulliGenerator()
 
-    runner = PLA(data)
-    logging.info('Accuracy: {0}%'.format(runner.calculate_accuracy() * 100))
+    runner = PLA(generator.generate(FLAGS.learning_points))
 
-    iteration = 0
-    while iteration < 1000:
+    for iteration in map(lambda x: x+1, range(FLAGS.num_iter)):
         runner.iterate()
 
-        iteration += 1
-        if iteration % 100 == 0:
-            logging.info('Accuracy: {0}%'.format(runner.calculate_accuracy() * 100))
-            #draw(runner.data)
+        if iteration % 50 == 0:
+            logging.info(
+                'Iter {0}: Accuracy => {1:.2f}%'.format(
+                    iteration,
+                    runner.calculate_accuracy() * 100))
 
-    logging.info('DONE Accuracy: {0}%'.format(runner.calculate_accuracy() * 100))
-    # plt.show()
+    logging.info('Final: weights => {0}'.format(runner.weights))
 
+    logging.info('====== INFERENCE ======')
+    logging.info('Num Data Points => {0}'.format(FLAGS.inference_points))
 
-def draw(data):
-    x_values = []
-    y_values = []
-    color_values = []
+    test_points = generator.generate(FLAGS.inference_points)
+    test_set_total = 0
+    test_set_correct = 0
+    for point in test_points:
+        predicted_target = runner.classify(point)
 
-    for point in data:
-        x, y = point.features
-        target = point.target
+        if predicted_target == point.target:
+            test_set_correct += 1
 
-        x_values.append(x)
-        y_values.append(y)
-        color_values.append('red' if target > 0 else 'blue')
+        test_set_total += 1
 
-    plt.scatter(x_values, y_values, c=color_values)
-    plt.show(block=False)
+    test_set_accuracy = test_set_correct / test_set_total
+    logging.info(
+        'Model Accuracy on Test Set => {0:.2f}%'.format(
+            test_set_accuracy * 100))
 
 
 if __name__ == '__main__':
